e025c1b9c29fa87cf0880ae4d9186b989273d242
an-hyeonjang <an.hyeonjang@gmail.com>
2020-06-10 16:46:42 +0900

    temp commit

--- cache_sim/main.cpp
+++ cache_sim/main.cpp
@@ -1,17 +1,22 @@
+#include <stdint.h>
+#include <cstdlib>
+#include <cstring>
 #include <fstream>
 #include <iostream>
 
 struct	inst_t
 {
 	char type;
-	uint32_t value;
-}
+	int32_t value;
+	
+	inst_t(){ type = NULL, value = NULL; };
 
-inst_t inst_mem[64];
+};
 
 int main(int argc, char** argv)
 {
-
+	inst_t inst_mem[64];
+	
 	std::string path = "./trace1.txt";
 
 
@@ -19,9 +24,17 @@ int main(int argc, char** argv)
 	if(file.is_open())
 	{
 		std::string line;
-		while(getline(file, line))
+		for(int i =0; i<sizeof(inst_mem), std::getline(file, line); i++)
 		{
-			std::cout << line << std::endl;
+			inst_mem[i].type = line[0];
+			std::cout << "origin " << line.substr(0, 10).c_str() << std::endl;
+			std::cout << "ato "<< std::atoll(line.substr(1, 10).c_str()) << std::endl;
+			inst_mem[i].value = std::atol(line.substr(1, -1).c_str());
+
+			memcpy(&inst_mem[i], &line[2], sizeof(int32_t));
+
+			printf("inst_mem %08x\n", inst_mem[i].value);
+			
 		}
 		file.close();
 	}
