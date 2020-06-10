ccfe07b3a59175ded95730bb8ba36da78234fb93
an-hyeonjang <an.hyeonjang@gmail.com>
2020-06-10 15:22:27 +0900

    temp commit

--- cache_sim/main.cpp
+++ cache_sim/main.cpp
@@ -1,6 +1,28 @@
+#include <fstream>
 #include <iostream>
 
-int main()
+struct	inst_t
 {
-	std::cout<<"project4"<<std::endl;
+	char type;
+	uint32_t value;
+}
+
+inst_t inst_mem[64];
+
+int main(int argc, char** argv)
+{
+
+	std::string path = "./trace1.txt";
+
+
+	std::ifstream file(path.data());
+	if(file.is_open())
+	{
+		std::string line;
+		while(getline(file, line))
+		{
+			std::cout << line << std::endl;
+		}
+		file.close();
+	}
 }
